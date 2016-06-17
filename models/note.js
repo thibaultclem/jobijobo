e54accd9ef6173d6ec8a1d846fe73e44d6b01ed1
ThibaultClement <thibault.clem@gmail.com>
2016-06-17 12:55:25 +0200

    Fix typo error in Note schemas

--- models/note.js
+++ models/note.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 
-var JobSchema = new mongoose.Schema({
+var NoteSchema = new mongoose.Schema({
   body: String,
   createdDate: Date,
   updatedDate: Date,
   job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' }
 });
 
-mongoose.model('Job', JobSchema);
+mongoose.model('Note', NoteSchema);
