630a95c58ca97d59bff07616599fe1b13edd5558
ThibaultClement <thibault.clem@gmail.com>
2016-07-07 15:59:46 +0800

    Use mongoose middleware method for cascading delete

--- models/job.js
+++ models/job.js
@@ -1,4 +1,8 @@
 var mongoose = require('mongoose');
+//model
+//var Contact = mongoose.model('Contact');
+var Status = mongoose.model('Status');
+var Note = mongoose.model('Note')
 
 var JobSchema = new mongoose.Schema({
   user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
@@ -12,6 +16,14 @@ var JobSchema = new mongoose.Schema({
   status: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Status' }],
   contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }],
   notes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }]
+});
+
+JobSchema.pre('remove', function(next) {
+  console.log('pre remove !!!');
+  Status.remove({job: this._id}).exec();
+  Note.remove({job: this._id}).exec();
+  //Contacts.remove({job: this._id}).exec();
+  next();
 });
 
 mongoose.model('Job', JobSchema);
--- models/status.js
+++ models/status.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var StatusSchema = new mongoose.Schema({
   name: String,
   createdDate: String,
-  previous: { type: mongoose.Schema.Types.ObjectId, ref: 'Status' },
   job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' }
 });
 
